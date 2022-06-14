2fab97ddac875174dac463c7360c8143cd6d0df8
SyedHamzaAsim <hamza.asim090@gmail.com>
2022-06-14 06:05:26 +0000

    a table of shortened urls now

--- express_server.js
+++ express_server.js
@@ -28,8 +28,13 @@ app.get("/hello", (req, res) => {
 app.get("/set", (req, res) => {
   const a = 1;
   res.send(`a = ${a}`);
- });
- 
- app.get("/fetch", (req, res) => {
+});
+
+app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
- });
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
